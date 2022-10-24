53d9daa00a845af5d11c038c70f2b4c4fa16d446
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-10-24 19:03:51 +0300

    goit-js-hw-06

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const newIngredients = document.querySelector("#ingredients")
+function addIngredients(name){
+  const list = document.createElement("li");
+  list.classList.add("item");
+  list.textContent = String(name);
+  newIngredients.append(list);
+  return list;
+}
+const s = ingredients.map(el => addIngredients(el));
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,7 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const newGallery = document.querySelector(".gallery");
+const addPages = images.map(({url, alt}) => `<li><img src="${url}" alt="${alt}"></li>`).join("");
+
+newGallery.insertAdjacentHTML("afterbegin",addPages);
