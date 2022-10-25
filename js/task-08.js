50ef8d69983989c6cb294b6bf0eaee2421f46210
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-10-25 19:42:50 +0300

    main

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const handelSubmitEmail = document.querySelector('.login-form')
+const obj ={};
+handelSubmitEmail.addEventListener("submit",even =>{
+    even.preventDefault();
+    const {elements : {email,password}} = even.target;
+    if (email.value === "" && password.value === "") {
+       return alert('Всі поля повинні бути заповнені');
+    }
+    obj[email.name] = email.value;
+    obj[password.name] = password.value;
+    even.currentTarget.reset();
+});
+console.log(obj);
